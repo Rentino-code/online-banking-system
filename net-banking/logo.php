45b7f70694d1186f334462c1dcbc3336d7a0ddce
Rentino Rentino <rentino744@gmail.com>
2024-11-16 10:04:18 +0300

    Update the change

---
+++ net-banking/logo.php
@@ -0,0 +1,17 @@
+<!-- logo.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="logo_style.css">
+</head>
+<body>
+    <div class="flex-container">
+        <div class="logo-container">
+            <img src="Natwest logo.svg" alt="NatWest Logo">
+        </div>
+    </div>
+</body>
+</html>
